bf4a0dd6b991379c874dbf730e20854ed53afaaa
mohammedLVM <mohammad@lvmodel.com>
2025-02-22 15:26:32 +0530

    summary also going

--- lib/ai/tools/creditcoin/get-creditcoin-api-docs.ts
+++ lib/ai/tools/creditcoin/get-creditcoin-api-docs.ts
@@ -14,26 +14,24 @@ export const getCreditcoinApiDoc = tool({
         "https://raw.githubusercontent.com/blockscout/blockscout-api-v2-swagger/main/swagger.yaml"
       );
       const allPaths = await getAllPaths(openapidata);
-      const allPathString = allPaths.join(", ");
 
       const response = await generateText({
         model: myProvider.languageModel("chat-model-small"),
         system: `\n
-        You will return the name of the api endpoint in the given list of api endpoint, which can be helpfull to answers user query. Do not modify it in any way.`,
+        You will just return the name of the api endpoint in the given list of api endpoint and their summary, which can be helpfull to answers user query. Do not modify it in any way.`,
         prompt: JSON.stringify(
-          `the list of api endpoints are ${allPathString} and user Query is ${userQuery}`
+          `The list of api endpoints and their summary are ${allPaths} and user Query is ${userQuery}`
         ),
       });
       const apiEndpointName = response.steps[0].text;
-      // console.log("api endpoint name choosen ------------ ", apiEndpointName);
+      console.log(
+        `AI selected the api endpoint as https://creditcoin.blockscout.com/api/v2${apiEndpointName}`
+      );
 
       const apiEndpointInfo = await getPathInfo(openapidata, apiEndpointName);
+      // console.log("apiEndpointInfo is -------- ", apiEndpointInfo);
       const apiEndpointInfoString = JSON.stringify(apiEndpointInfo);
       // console.log("apiEndpointInfoString -------- ", apiEndpointInfoString);
-
-      console.log(
-        `the api endpoint selected is https://creditcoin.blockscout.com/api/v2${apiEndpointName}`
-      );
 
       return `the api endpoint you should call is-  https://creditcoin.blockscout.com/api/v2${apiEndpointName}  - and info about the endpoint is - ${apiEndpointInfoString}`;
     } catch (error) {
