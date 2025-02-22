4119d97bd4858fabf20b407903af724b7f804c88
mohammedLVM <mohammad@lvmodel.com>
2025-02-22 20:41:46 +0530

    maskable icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,37 +1,40 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Javin.ai',
-    short_name: 'Javin.ai',
-    description: 'A focused, no-nonsense AI search engine for crypto and blockchain.',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
+    name: "Javin.ai",
+    short_name: "Javin.ai",
+    description:
+      "A focused, no-nonsense AI search engine for crypto and blockchain.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
     icons: [
       {
-        src: '/images/javin/javin_192px_text.jpg',
-        sizes: '192x192',
-        type: 'image/jpg',
+        src: "/images/javin/javin_192px_text.jpg",
+        sizes: "192x192",
+        type: "image/jpg",
+        purpose: "maskable",
       },
       {
-        src: '/images/javin/javin_512px_text.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/images/javin/javin_512px_text.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
     screenshots: [
       {
-        src: '/images/javin/screenshots/Screenshot_400x800.png',
-        sizes: '400x800',
-        type: 'image/png',
+        src: "/images/javin/screenshots/Screenshot_400x800.png",
+        sizes: "400x800",
+        type: "image/png",
       },
       {
-        src: '/images/javin/screenshots/Screenshot_1280x960.png',
-        sizes: '1280x960',
-        type: 'image/png',
-      }
+        src: "/images/javin/screenshots/Screenshot_1280x960.png",
+        sizes: "1280x960",
+        type: "image/png",
+      },
     ],
-  }
+  };
 }
