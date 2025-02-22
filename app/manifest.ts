601842af52ab94da8b012257201d5dd42c61b3d4
mohammedLVM <mohammad@lvmodel.com>
2025-02-22 22:34:54 +0530

    renamed icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,13 +12,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/images/javin/javin_192px_text.jpg",
+        src: "/images/javin/javin_192px_text1.jpg",
         sizes: "192x192",
         type: "image/jpg",
         purpose: "maskable",
       },
       {
-        src: "/images/javin/javin_512px_text.jpg",
+        src: "/images/javin/javin_512px_text1.jpg",
         sizes: "512x512",
         type: "image/jpg",
         purpose: "maskable",
