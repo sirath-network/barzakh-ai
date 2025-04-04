a601a1019e962492c841e945539d8fcbef5879fe
lvmShantanu <shantanu@lvmodel.com>
2025-04-04 09:08:19 +0530

    hydration error fixed

--- apps/javin/components/overview.tsx
+++ apps/javin/components/overview.tsx
@@ -1,9 +1,15 @@
 import { motion } from "framer-motion";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export const Overview = () => {
   const { resolvedTheme } = useTheme();
-  console.log(resolvedTheme);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <motion.div
       key="overview"
@@ -14,20 +20,16 @@ export const Overview = () => {
       transition={{ delay: 0.5 }}
     >
       <div className="rounded-xl p-6 flex flex-col items-center gap-2 leading-relaxed text-center max-w-2xl">
-        {resolvedTheme == "dark" ? (
+        {isMounted && (
           <img
             alt="Javin.ai"
-            src="/images/javin/banner/javin-banner-white.svg"
-            className=" w-32 sm:w-48 h-auto"
+            src={
+              resolvedTheme === "dark"
+                ? "/images/javin/banner/javin-banner-white.svg"
+                : "/images/javin/banner/javin-banner-black.svg"
+            }
+            className="w-32 sm:w-48 h-auto"
           />
-        ) : (
-          resolvedTheme == "light" && (
-            <img
-              alt="Javin.ai"
-              src="/images/javin/banner/javin-banner-black.svg"
-              className=" w-32 sm:w-48 h-auto"
-            />
-          )
         )}
         <p className="text-lg text-muted-foreground">
           A focused, no-nonsense AI search engine for crypto.
